ce36b307c4a4ad5e094fe852c667bfc884151e0a
Carlo Hamalainen <carlo@carlo-hamalainen.net>
2016-04-10 22:33:08 +0800

    Model for structured log items.

--- structlog/models.py
+++ structlog/models.py
@@ -1,5 +1,27 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.postgres.fields import HStoreField
 
-# Create your models here.
+class LogItem(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # e.g. 'rdiff-backup'
+    name = models.CharField(max_length=200)
+
+    # e.g. 'my-server-1'
+    host = models.CharField(max_length=200)
+
+    # e.g. 'carlo'
+    user = models.CharField(max_length=200)
+
+    # Maybe something nice for a graph or email subject line.
+    # e.g. 'daily rdiff-backup of something'
+    description = models.TextField()
+
+    # key/value blob
+    attributes = HStoreField()
+
+    def __str__(self):
+        return self.name
